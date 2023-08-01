424500bda9c1a35ec14b15606289509146361857
Guilherme Magalhães <guigsgbm@gmail.com>
2023-08-01 13:15:19 -0300

    Data Publicacao updated in frontend

--- WebAPI/Dtos/CreateNoticeDto.cs
+++ WebAPI/Dtos/CreateNoticeDto.cs
@@ -2,9 +2,9 @@
 
 public class CreateNoticeDto
 {
-    public string Titulo { get; set; }
+    public string? Titulo { get; set; }
 
-    public string Descricao { get; set; }
+    public string? Descricao { get; set; }
 
-    public string Autor { get; set; }
+    public string? Autor { get; set; }
 }
